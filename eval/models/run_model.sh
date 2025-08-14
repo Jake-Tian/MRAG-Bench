abd4aa4a4e766627ccd4fd2a3a18cd18b72a29b5
Jake-Tian <1627042958@qq.com>
2025-08-14 14:51:45 +0800

    update run_model.sh

--- eval/models/run_model.sh
+++ eval/models/run_model.sh
@@ -2,9 +2,9 @@
 # export HF_HOME="./cache"
 # export HF_HUB_CACHE="./cache"
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=2
 
-python3 eval/models/clip_model.py \
-    --answers-file "clip_model_gt_rag_results.jsonl" \
+python3 eval/models/llava_one_vision.py \
+    --answers-file "llava_one_vision_gt_rag_results.jsonl" \
     --use_rag True \
     --use_retrieved_examples False 
